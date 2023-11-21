3cff9fa49ac9e161f90381ff925308094826d581
gpBlockchain <744158715@qq.com>
2023-11-21 20:00:18 +0800

    add dep

--- prepare.sh
+++ prepare.sh
@@ -6,4 +6,5 @@ git clone https://github.com/gpBlockchain/ckb-rpc-mock-data.git
 cd ckb-rpc-mock-data
 git checkout v0.111.0
 pip install -r requirements.txt
+pip install Werkzeug
 python3 api/index.py > index.log 2>&1 &

e644e7f8c22d05d3a72b197bcb3e5d8697d619bc
gpBlockchain <32102187+gpBlockchain@users.noreply.github.com>
2023-11-21 19:25:40 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -4,5 +4,6 @@ git checkout ckb2023
 cd ../
 git clone https://github.com/gpBlockchain/ckb-rpc-mock-data.git
 cd ckb-rpc-mock-data
+git checkout v0.111.0
 pip install -r requirements.txt
 python3 api/index.py > index.log 2>&1 &

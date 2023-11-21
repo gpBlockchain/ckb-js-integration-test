408e0bf81f72f4ee22fdf62af80ff775be14d655
gpBlockchain <744158715@qq.com>
2023-11-21 20:04:30 +0800

    debug py

--- prepare.sh
+++ prepare.sh
@@ -6,5 +6,7 @@ git clone https://github.com/gpBlockchain/ckb-rpc-mock-data.git
 cd ckb-rpc-mock-data
 git checkout v0.111.0
 pip install -r requirements.txt
-pip install Werkzeug
+pip install Werkzeug==2.2.2
 python3 api/index.py > index.log 2>&1 &
+sleep 5
+cat index.log
